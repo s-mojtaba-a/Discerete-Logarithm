5874bb945460544d2ffb691557ac0ac5b3ec4089
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-10-14 00:12:49 +0330

    Add files via upload
    Source code

---
+++ Cpp.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(), (v).end()
+#define debug(x) cout << #x << " = " << x << endl
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+inline ll Mod(ll x, ll mod) { return x % mod >= 0 ? x % mod : x % mod + mod; }
+
+template <typename T>
+T power(T x, T y, T n)
+{
+    x %= n;
+    T ans = 1;
+    while (y > 0)
+    {
+        if (y & 1 == 1)
+        {
+            ans = ans * x % n;
+        }
+        x = x * x % n;
+        y >>= 1;
+    }
+    return (ans);
+}
+
+template <typename T>
+T solve(T a, T b, T m)
+{
+    a %= m, b %= m;
+    T k = 1, add = 0, g;
+    while ((g = __gcd(a, m)) > 1)
+    {
+        if (b == k)
+            return add;
+        if (b % g)
+            return -1;
+        b /= g, m /= g, add++;
+        k = (k * 1ll * a / g) % m;
+    }
+
+    T n = sqrt(m) + 1;
+    T an = power(a, n, m);
+
+    unordered_map<T, T> vals;
+    for (T q = 0, cur = b; q <= n; q++)
+    {
+        vals[cur] = q;
+        cur = (cur * 1ll * a) % m;
+    }
+
+    for (T p = 1, cur = k; p <= n; p++)
+    {
+        cur = (cur * 1ll * an) % m;
+        if (vals.count(cnt))
+        {
+            T ans = n * p - vals[cur] + add;
+            return ans;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
---
+++ python.py
@@ -0,0 +1,73 @@
+from math import sqrt, gcd
+
+
+class DL:
+    ''' a^x = b (mod m)
+        a and m may be coprime or not
+    '''
+
+    def __init__(self):
+        self.Q = {}
+
+    def power(self, x, y, n):
+        x %= n
+        ans = 1
+        while y > 0:
+            if y & 1 == 1:
+                ans = ans*x % n
+            x = x*x % n
+            y >>= 1
+        return(ans)
+
+    def find_q(self, a, b, m, n):
+        ans = b
+        for i in range(0, n+1):
+            self.Q[ans] = i
+            ans = ans*a % m
+
+    def find_add_and_k(self, a, b, m):
+        ''' it may return the solution or it may say that there is no solution '''
+        k = 1
+        add = 0
+        while (gcd(a, m) > 1):
+            g = gcd(a, m)
+            if b == k:
+                return (-1, add)
+            if b % g:
+                return (-1, -1)
+            b //= g
+            m //= g
+            k = k*(a/g) % m
+            add += 1
+        return (k, add)
+
+    def solve(self, a, b, m):
+        ''' it returns the smallest x  '''
+        a %= m
+        b %= m
+        n = int(sqrt(m))+1
+        k, add = self.find_add_and_k(a, b, m)
+        if add == -1:
+            return -1
+        if k == -1:
+            return add
+        self.find_q(a, b, m, n)
+        an = self.power(a, n, m)
+        temp = k*an
+        for p in range(1, n+1):
+            if self.Q.get(temp, None) != None:
+                return ((p*n-self.Q[temp]+add) % m+m) % m
+            temp = temp*an % m
+        return -1
+
+
+solver = DL()
+while True:
+    a, m, b = map(int, input().split())
+    if a == b == m == 0:
+        break
+    ans = solver.solve(a, b, m)
+    if ans == -1:
+        print('No Solution')
+    else:
+        print(ans)
